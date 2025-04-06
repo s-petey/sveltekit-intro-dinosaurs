46e411c5942d792940f981e994864eb6df174acb
Sam Peterson <samllama55@gmail.com>
2025-04-06 22:33:03 +0200

    Initial commit

---
+++ src/lib/server/db/schema.ts
@@ -0,0 +1,16 @@
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+
+export const user = sqliteTable('user', {
+	id: integer('id').primaryKey(),
+	name: text('name')
+});
+
+export const dinosaur = sqliteTable('dinosaur', {
+	id: integer('id').primaryKey(),
+	name: text('name'),
+	description: text('description')
+});
+
+export type Dinosaur = typeof dinosaur.$inferSelect;
+export type DinosaurInsert = typeof dinosaur.$inferInsert;
+export type User = typeof user.$inferSelect;
---
+++ src/routes/+page.server.ts
@@ -0,0 +1,15 @@
+import { type Actions } from '@sveltejs/kit';
+import { runSeed } from '../lib/server/db/seed';
+import { db } from '$lib/server/db';
+import { dinosaur } from '$lib/server/db/schema';
+
+export const load = async () => {
+	const first10 = await db.select().from(dinosaur).limit(10);
+	return { dinosaurs: first10 };
+};
+
+export const actions: Actions = {
+	default: async ({ request }) => {
+		await runSeed();
+	}
+};
